d2ba61fc55b1e3000043270fab65560e4f06c8c4
Kristopher Parrillo <kkparrillo@gmail.com>
2017-01-07 19:14:05 -0800

    the submit!

--- server.js
+++ server.js
@@ -18,12 +18,35 @@ app.get('/off_with_their?:part', function(req, res) {
   var part = req.query.part;
   var html = '<h1>Off with their ' + part +' !</h1>';
   res.send( html );
+});
+
+app.get('/characters/hatter', function(req, res) {
+  var html = '<h1>have a cuppa</h1>'
+  res.send( html );
+});
+
+app.get('/characters/queen', function(req, res){
+  res.redirect('/off_with_their?part=head');
 })
 
+app.get('/elevator', function(req, res){
+  var html = '<form action="/elevator"><input type="text" name="floor"><input type="submit" value="Go!"></form>';
+  res.send( html );
+})
 
+app.get('/elevator?floor', function(req, res) {
+  var number = req.query.floor;
+  var floor = req.params.floor;
+  res.redirect('/floor');
+  // res.redirect('/floor' + floor + number );
+})
 
+// var html = '<h1>Floor number is ' + floor + '</h1>';
 
-
+// app.get('/characters/cheshire_cat', function(req,res){
+//   var html = '<form><input type="text"><br><input type="radio" name="radio" value="Show" checked>Show <input type="radio" name="radio" value="Hide">Hide <input type="submit" value="submit"></form>'
+//   res.send( html );
+// })
 
 
 
