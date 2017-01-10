5c8fe601c6d05c93cf938678c3a3cf05c9568150
Kristopher Parrillo <kkparrillo@gmail.com>
2017-01-09 21:52:43 -0800

    working

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', function(req, res){
   var greet = '<h1>Hello and Welcome!</h1><br/>';
@@ -30,26 +35,41 @@ app.get('/characters/queen', function(req, res){
 })
 
 app.get('/elevator', function(req, res){
-  var html = '<form><input type="text" name="number"><input type="submit" value="Go!"></form>';
+  var html = '<form action="/floor"><input type="text" name="number"><input type="submit" value="Go!"></form>';
   res.send( html );
 })
 
-app.get('/elevator?:floor', function(req, res) {
-  var number = req.query.floor;
+app.get('/floor', function(req, res) {
+  var number = req.query.number;
   var floor = req.params.floor;
-  res.redirect('/floor');
-  // res.redirect('/floor' + floor + number );
+  res.send('Welcome to floor ' + number );
 })
 
-// var html = '<h1>Floor number is ' + floor + '</h1>';
 
 app.get('/characters/cheshire_cat', function(req,res){
-  var html = '<form><input type="text"><br><input type="radio" name="radio" value="Show" checked>Show <input type="radio" name="radio" value="Hide">Hide <input type="submit" value="submit"></form>'
+  var html = '<form method="POST" action="/dumb_waiter"><input type="text"><br><input type="radio" name="radio" value="Show" checked>Show <input type="radio" name="radio" value="Hide">Hide <input type="submit" value="submit"></form>'
   res.send( html );
 })
 
+app.post('/dumb_waiter', function(req, res) {
+  var value = req.body.radio;
+  if (value === 'Show') {
+    return res.redirect('/curious?visit=show')
+  } else if (value === 'Hide') {
+    return res.redirect('/curious?visit=hide')
+  }
+});
 
-
+app.get('/curious', function(req, res) {
+  var showHTML = '<h1>=^..^=</h1>';
+  var hideHTML = '<h1>:)</h1>';
+  var value = req.query.visit;
+  if (value === 'show') {
+    return res.send( showHTML );
+  } else if (value === 'hide') {
+    return res.send( hideHTML );
+  }
+});
 
 
 
